9d459022b9df41cc1e883cc39c33e14482cad8ba
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2022-04-07 11:00:55 +0200

    Update style.js

--- style.js
+++ style.js
@@ -38,13 +38,13 @@
 				{ name: 'Caja 03', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-3' } },
 				{ name: 'Caja 04', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-4' } },
 
-				{ name: 'Tabla naranja', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-1'} },
-				{ name: 'Tabla rosa', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-2'} },
-				{ name: 'Tabla verde', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-3'} },
+				{ name: 'Tabla 01', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-1'} },
+				{ name: 'Tabla 02', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-2'} },
+				{ name: 'Tabla 03', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-3'} },
 				
-				{ name: 'Celda 2', element: 'td', attributes: { 'class': 'bck-td-2'} },
-				{ name: 'Celda 3', element: 'td', attributes: { 'class': 'bck-td-3'} },
-				{ name: 'Celda 4', element: 'td', attributes: { 'class': 'bck-td-4'} },
+				{ name: 'Celda 02', element: 'td', attributes: { 'class': 'bck-td-2'} },
+				{ name: 'Celda 03', element: 'td', attributes: { 'class': 'bck-td-3'} },
+				{ name: 'Celda 04', element: 'td', attributes: { 'class': 'bck-td-4'} },
 				
 				{ name: 'Icono Hablar', element: 'span', attributes: { 'class': 'icon icon-hablar' } },
 
